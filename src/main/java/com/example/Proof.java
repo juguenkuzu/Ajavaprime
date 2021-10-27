9d1a6b7a8da8a5d2d942512cdffa1c1560cde9d5
juguenkuzu <juguenkuzu@gmail.com>
2021-10-27 13:39:41 -0400

    ok2

--- src/main/java/com/example/Proof.java
+++ src/main/java/com/example/Proof.java
@@ -3,7 +3,7 @@ package com.example;
 public class Proof {
 
 	public static void main(String[] args) {
-	System.out.println("Hello Jenkins");
+	System.out.println("Jenkins is a build automation tool ,CI/CD tool  ");
 
 	}
 
